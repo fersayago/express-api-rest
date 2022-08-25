b3a4afd137a952d4a83536de464cf20d498a5b65
fersayago <sayago.fernando@gmail.com>
2022-08-25 20:07:02 -0300

    Routing con express

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy un nuevo endpoint');
+})
+
+
+// lo que mas vamos a devolver son json ya que vamos a hacer una API
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: '1000',
+    description: 'Descripcion del producto 1'
+  });
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
