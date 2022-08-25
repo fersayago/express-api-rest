03109475e31d74294ec1f5169030795419b0b447
fersayago <sayago.fernando@gmail.com>
2022-08-25 20:20:40 -0300

    GET: recibir parametros

--- index.js
+++ index.js
@@ -12,12 +12,43 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy un nuevo endpoint');
 })
 
-
-// lo que mas vamos a devolver son json ya que vamos a hacer una API
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 100,
+      description: 'Descripcion del producto 1'
+    },
+    {
+      name: 'Producto 2',
+      price: 200,
+      description: 'Descripcion del producto 2'
+    },
+    {
+      name: 'Producto 3',
+      price: 300,
+      description: 'Descripcion del producto 3'
+    }
+  ]);
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Producto 1',
-    price: '1000',
+    price: 100,
+    description: 'Descripcion del producto 1'
+  });
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+    name: 'Producto 1',
+    price: 100,
     description: 'Descripcion del producto 1'
   });
 })
