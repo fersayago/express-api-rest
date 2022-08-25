8c0f21314e50a771f6c90d17d560960d3695ebe5
fersayago <sayago.fernando@gmail.com>
2022-08-25 20:00:13 -0300

    instalacion de express y primer servidor HTTP

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3005;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
