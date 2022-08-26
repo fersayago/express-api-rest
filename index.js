4b9fdd288662fee6f807c75fc33d28322f1e98f2
fersayago <sayago.fernando@gmail.com>
2022-08-25 23:49:51 -0300

    GET: parametros query y evitar choque de rutas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 
@@ -12,25 +13,33 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy un nuevo endpoint');
 })
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 1',
-      price: 100,
-      description: 'Descripcion del producto 1'
-    },
-    {
-      name: 'Producto 2',
-      price: 200,
-      description: 'Descripcion del producto 2'
-    },
-    {
-      name: 'Producto 3',
-      price: 300,
-      description: 'Descripcion del producto 3'
-    }
-  ]);
+// ! TODA RUTA ESPECIFICA DEBE IR ANTES QUE LA RUTA DINAMICA
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 })
+
+app.get('/products', (req, res) => {
+  const size = req.query.size;
+  const products = [];
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.image(),
+    })
+  }
+
+  res.json(products);
+})
+
+// ! TOMA FILTER COMO IDENTIFICADOR DE PRODUCTO YA QUE LAS RUTAS CHOCAN
+/* // ? SE TIENE QUE DECLARAR ANTES
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
+})
+ */
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
@@ -40,6 +49,19 @@ app.get('/products/:id', (req, res) => {
     price: 100,
     description: 'Descripcion del producto 1'
   });
+})
+
+app.get('/users',(req, res) => {
+  const {limit, offset} = req.query;
+  // como son opcionales deberia realizar una validacion
+  if( limit && offset ){
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
