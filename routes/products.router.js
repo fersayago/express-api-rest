9538690da5609ac159d915ed46dcb6df9869373a
fersayago <sayago.fernando@gmail.com>
2022-08-26 04:04:39 -0300

    PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -41,4 +41,22 @@ router.post('/', (req, res) => {
   })
 })
 
+// hace un update de solamente los campos que se le pasan en el body
+router.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  res.json({
+    message: 'partial update',
+    data: body,
+    id,
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  res.json({
+    message: `${id} deleted`
+  })
+})
+
 module.exports = router;
