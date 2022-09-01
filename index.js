535c717ed57b162c1b8dd52b4b8cbaf424288374
fersayago <sayago.fernando@gmail.com>
2022-09-01 03:11:27 -0300

    CORS config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi = require('./routes/index');
 
 const { boomErrorHandler, logErrors, errorHandler } = require('./middlewares/error.handler')
@@ -9,6 +10,21 @@ const port = 3005;
 
 // Middleware
 app.use(express.json());
+
+// habilitamos a cualquier dominio de origen en todo el proyecto
+//app.use(cors());
+const whitelist = ['http://localhost:3000', 'https://fersayago.github.io'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)){ // si el origen esta en la whitelist
+      callback(null, true); // no hay error y el acceso esta permitido
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options))
+
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
